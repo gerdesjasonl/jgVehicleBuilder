cc8cd22931d8995f75ce416af673ffb8a7584026
gerdesjasonl <gerdesjasonl@gmail.com>
2025-01-23 17:39:08 -0500

    This fixes the bug where trucks could tow themselves. Message now appears saying 'You cannot tow yourself!'

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -295,7 +295,7 @@ class Cli {
         // TODO: check if the selected vehicle is the truck
         // TODO: if it is, log that the truck cannot tow itself then perform actions on the truck to allow the user to select another action
         // TODO: if it is not, tow the selected vehicle then perform actions on the truck to allow the user to select another action
-          if (answers.vehicleToTow === 'Truck') {
+          if (answers.vehicleToTow instanceof Truck) {
             console.log('You cannot tow yourself!');
             this.findVehicleToTow(truck);
           }else{
